e09078f4abec84ea727c36bb500d2c522951b301
Asiya-7387 <asiyashaikh7835@gmail.com>
2025-05-08 22:45:28 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,108 @@
+# 1.write a python program to enter two numbers and find their sum
+
+# a=int(input('enter 1st number'))
+# b=int(input('enter 2 nd number'))
+# sum=a+b
+# print(sum) 
+#--------------------------------------------------------------
+# 2.write a python program to enter two numbers and perform all arithmatic operation
+
+# a=int(input ('enter 1st number'))
+# b=int(input('enter 2nd number'))
+# add=a+b
+# print("addition is:",add)
+
+# sub=a-b
+# print("substraction is:",sub)
+
+# mul=(a*b)
+# print("multiplication is:",mul)
+
+# div=(a/b)
+# print("division is:",div)
+
+# print("floor division is:",a//b )
+
+# print("exponentitation is:",a**b)
+
+# print("reminder is:",a%b)
+
+#---------------------------------------------------------------------
+# 3. WAP TO ENTER LENGTH AND BREADTH OF REACTANGLE AND FIND ITS AREA
+
+# l=int(input("enter the length"))
+# b=int(input("enter the breadth"))
+# area=l*b
+# perimeter=2*(l+b)
+# print("Area of reactangle is:", area)  
+# print("perimeter of reactangle is",perimeter) 
+#-------------------------------------------------------
+# 4.WAP TO ENTER BASE AND HIGHT OF TRIANGLE AND FIND ITS AREA
+
+# base=int(input("enter base"))
+# height=int(input("enter height"))
+# area=(base*height)/2
+# print("Area of triangle is:",area)
+#-------------------------------------------------------------
+# 5.WAP TO CALCULATE SQUARE OF GIVEN NUMBER
+
+# n=6
+# sqr=n*n
+# print("square of given number is:",sqr)
+#-------------------------------------------------------------------
+# 6.WAP TO CALCULATE CUBE OF GIVEN NUMBER
+
+# n=int(input("enter number"))
+# cube=n*n*n
+# print("cube of given number is",cube )
+#-------------------------------------------------------------
+# 7.write  a python program to enter P,T,R and calculate simple interest
+
+# P=int(input("enter principle"))
+# T=int(input("enter time"))
+# R=int(input("enter rate"))
+
+# SI=P*R*T/100
+
+# print("simple interest",SI)
+#------------------------------------------------------------------
+# 8.SWAP VALUES OF TWO INTEGER
+
+# a=int(input("enter number"))
+# b=int(input("enter number"))
+
+# temp=a
+# a=b
+# b=temp
+
+# print("swap of two integer value is:",a, b)
+#------------------------------------------------------
+
+# write a python program to enter marks of five subjects and calculate total ,
+#average and percentage
+
+m1=int(input("enter marks of 1st subject"))
+m2=int(input("enter marks of 2nd subject"))
+m3=int(input("enter marks of 3rd subject"))
+m4=int(input("enter marks of 4th subject"))
+m5=int(input("enter marks of 5th subject"))
+
+total=m1+m2+m3+m4+m5
+
+print("Total of five subject is:",total)
+
+average=total/5
+
+print("average of five subject is:",average)
+
+percentage=(total*100)/500
+
+print("percentage of five subject is",percentage)
+
+
+
+
+
+
+      
+
