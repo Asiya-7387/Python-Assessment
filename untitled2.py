d91e775e36cde61b6d5266a3ef7270a5172f2985
Asiya-7387 <asiyashaikh7835@gmail.com>
2025-05-08 22:34:08 +0530

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,39 @@
+#create simple addition function to accept 2 number &display sum
+
+# def addition():
+#    a=int(input('enter value'))
+#    b=int(input('enter value'))
+#    c=a+b
+#    print("addition is",c)
+    
+# execution/call    
+
+# addition()
+# i=1
+# while i<=5:
+#     addition()
+#     i+=1
+
+# def substraction():
+#     a=int(input("enter 1st value"))
+#     b=int(input("enter 2nd value"))
+#     c=a-b
+#     print("substraction is",c)
+    
+# substraction()
+# i=1
+# while i<=2:
+#      substraction() 
+#      i+=1
+
+def multiplication():
+    a=int(input("enter 1st value"))
+    b=int(input("enter 2nd value"))
+    c=a*b
+    print("multiplication is",c)
+    
+multiplication()
+i=1
+while i<=2:
+      multiplication() 
+      i+=1
