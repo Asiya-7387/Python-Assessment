3b9fe509ae54e66b7df66463407ab67fc4535a7d
Asiya-7387 <asiyashaikh7835@gmail.com>
2025-05-08 22:47:23 +0530

    Add files via upload

---
+++ if_else assignment.py
@@ -0,0 +1,27 @@
+#1.WAP TO ACCEPT YEAR FROM USER & CHECK YEAR IS LEAP YEAR OR NOT
+
+# year=int(input("enter year"))
+
+# if(year%4==0 or (year%100==0 and year%400==0)):
+#     print("year is leap year")
+# else:
+#     print("year is not leap year")    
+    
+# 2.WAP TO CHECK ENTERED NUMBER IS DIVISIBLE BY 5OR NOT
+
+# num=int(input("enter number")) 
+
+# if(num%5==0):
+#     print("number is divisible by 5" ) 
+# else:
+#     print("number is not divisible by 5")    
+   
+# 3.wap to accept a number from user &print the last digit
+     
+# num=int(input("enter any number:"))
+
+# if num>=10:
+#    last_digit=num%10
+# print("last digit:",last_digit)  
+
+ 
