007a534cbd9f3f18dff78e854c2bfb25db0045d5
Asiya-7387 <asiyashaikh7835@gmail.com>
2025-05-09 10:11:57 +0530

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,19 @@
+#  if...elif
+
+# percentage=int(input('Enter percentage'))
+# h
+# if percentage>80:
+#     print("Grade A+")
+# elif percentage>60 and percentage <=80  :
+    
+# user select the option    
+
+sugar="true"
+milk="true"
+expresso="true"
+tea_powder="false"
+
+print("to make coffee/Tea")
+
+if sugar:
+    print("Add sugar")
