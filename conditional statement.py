e0682e850c2dcf27fc4695b17dfcdcd607d12980
Asiya-7387 <asiyashaikh7835@gmail.com>
2025-05-10 15:15:36 +0530

    Add files via upload

--- conditional statement.py
+++ conditional statement.py
@@ -1,12 +1,28 @@
+# accept the number from user & CHECK NUMBER IS +VE,-VE,OR ZERO
+num= int(input("enter number"))
+if num>0:
+    print("+ve number")
+elif num<0:
+    print("-ve number")   
+else:
+    print("zero") 
+    
+    
 #  if...elif
 
-# percentage=int(input('Enter percentage'))
-# h
-# if percentage>80:
-#     print("Grade A+")
-# elif percentage>60 and percentage <=80  :
+percentage=int(input('Enter percentage'))
+
+if percentage>80:
+    print("Grade A+")
+elif percentage>60 and percentage <=80  :
+      print("Grade A")
+elif percentage>=40 and percentage<=60:
+     print("grade B")
+else:
+     print("you are fail")
+            
     
-# user select the option    
+# user select the option   to make coffee/tea  
 
 sugar="true"
 milk="true"
@@ -17,3 +33,10 @@ print("to make coffee/Tea")
 
 if sugar:
     print("Add sugar")
+if milk:
+    print("add milk")
+if expresso:
+   print("add expresso")
+if tea_powder:
+    print("add tea powder")
+    
