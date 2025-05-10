2c867202824714adbc02b0195d7e0e2f11064f65
Asiya-7387 <asiyashaikh7835@gmail.com>
2025-05-10 15:17:39 +0530

    Add files via upload

---
+++ string function.py
@@ -0,0 +1,12 @@
+s1='hello to all..'
+print(s1)
+
+s2="It's my first python session"
+print(s2)
+
+s3=''' THIS IS SAMPLE TEXT IN PARAGRAPH WHICH CONTAINS MULTIPLE LINES..'''
+print(s3)
+
+s4="""it's my first session with python current 
+      topic is string in python"""
+print(s4)      
